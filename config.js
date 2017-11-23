d6cb551ddc5845e6314fa0fa1667ac32a06a21fe
Neeraj Wadhwa <Neeraj_Wadhwa@Zalaks-MacBook-Pro.local>
2017-11-23 11:56:09 +0530

    minor bug fix

--- config.js
+++ config.js
@@ -8,11 +8,11 @@
 module.exports = {
 
     from: {
-        version: 'verison',
+        version: 'version',
         url: 'apigee url for source',
         userid: ' user email',
         passwd: 'user password',
-        org: 'suorce org',
+        org: 'source org',
         env: 'source env'
     },
     to: {
